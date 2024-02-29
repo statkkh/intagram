7919ee66f97768a21119d034122d2ca850e91791
kkh <qr21@naver.com>
2024-02-29 15:21:00 +0900

    2th commit feat : 이메일 중복에 대한 코드와 메시지 추가
    이메일 중복에 대한 코드와 메시지 추가

--- src/main/java/com/clone/instagram/dto/response/ResponseCode.java
+++ src/main/java/com/clone/instagram/dto/response/ResponseCode.java
@@ -4,5 +4,8 @@ public interface ResponseCode {
     
     String VALIDATION_FAILED = "VF";
     String SUCCESS = "SU";
-    String DATABASE_ERROR = "DBE";    
+    String DATABASE_ERROR = "DBE";  
+    
+    String DUPLICATED_EMAIL = "DE";    
+
 }
--- src/main/java/com/clone/instagram/dto/response/ResponseMessage.java
+++ src/main/java/com/clone/instagram/dto/response/ResponseMessage.java
@@ -5,4 +5,6 @@ public interface ResponseMessage {
     String VALIDATION_FAILED = "Validation Failed.";
     String SUCCESS = "Succss.";
     String DATABASE_ERROR = "DataBase error.";
+
+    String DUPLICATED_EMAIL = "Duplicated email.";
 }
